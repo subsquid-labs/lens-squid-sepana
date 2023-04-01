98ffb122430e394744f542196de3a450f55ccad4
Alexander Belopashentsev <abelopashentsev@gmail.com>
2023-04-01 13:22:42 +0500

    fix regex

--- src/processor.ts
+++ src/processor.ts
@@ -367,7 +367,7 @@ async function fetchMetadata(items: {id: string; contentURI: string | undefined 
                         } else {
                             metadata = await httpClient.get(url).catch(() => undefined)
                         }
-                    } else if (/[a-zA-Z0-9]/.test(url)) {
+                    } else if (/^[a-zA-Z0-9]+$/.test(url)) {
                         metadata = await ipfsClient.get('ipfs/' + url)
                     } else {
                         throw new Error(`Unexpected url "${url}"`)
